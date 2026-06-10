c6e375924a40fb5452925e585aa9376a1ab64559
Franny <derek@bloguin.com>
2026-06-10 19:21:21 -0400

    Add hosted trap trainer page

---
+++ src/app/trap-trainer/page.tsx
@@ -0,0 +1,11 @@
+export default function TrapTrainerPage() {
+  return (
+    <main className="min-h-screen bg-slate-950 p-4">
+      <iframe
+        src="/trap-trainer/index.html"
+        title="BetterChess Trap Trainer"
+        className="h-[95vh] w-full rounded-xl border border-slate-700 bg-white"
+      />
+    </main>
+  );
+}
